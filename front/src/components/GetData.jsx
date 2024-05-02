744d06205652808b6ed90c8ac851c6a78f07b3c9
Scott Liu <wbcwbc1616@gamil.com>
2024-05-02 13:19:21 +0800

    solcing useCallback dependency problem

--- front/src/components/GetData.jsx
+++ front/src/components/GetData.jsx
@@ -1,9 +1,29 @@
 import { useCallback, useEffect, useState } from "react";
 
-const GetData = (fs, url, item, itemName, itemCode) => {
+const GetData = (url, item, itemName, itemCode) => {
   const [datas, setDatas] = useState("");
   const executeFetch = useCallback(() => {
-    fs(setDatas, url, item, itemName, itemCode);
+    function getXml(setState, apiurl, item, itemName, itemCode) {
+      var xml = new XMLHttpRequest();
+      xml.onload = function () {
+        var xmldom = new DOMParser();
+        var data = xmldom.parseFromString(this.responseText, "text/xml");
+
+        let list = Array.from(data.getElementsByTagName(item)).map((item) => {
+          let Code =
+            itemCode == null
+              ? null
+              : item.getElementsByTagName(itemCode)[0].textContent;
+          let Name = item.getElementsByTagName(itemName)[0].textContent;
+          return itemCode == null ? { Name } : { Code, Name };
+        });
+
+        setState(list);
+      };
+      xml.open("GET", apiurl);
+      xml.send();
+    }
+    getXml(setDatas, url, item, itemName, itemCode);
   }, [url, item, itemName, itemCode]);
   useEffect(() => {
     executeFetch();
